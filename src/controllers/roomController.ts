713013ffd176ec545cd5ff33ba9dabc9f8f2a24d
JuanLuisAvilaCervera <juanluisavilacervera44@gmail.com>
2025-05-03 11:18:55 +0200

    Cambios mango

--- src/controllers/roomController.ts
+++ src/controllers/roomController.ts
@@ -1,74 +1,74 @@
-import { Request , Response, Router} from 'express';
-import Rooms from '../data/Rooms.json'
-import { RoomService } from '../services/roomService';
-import { RoomValidator } from '../validators/roomValidator';
-import { IdValidator } from '../validators/idValidator';
+// import { Request , Response, Router} from 'express';
+// import Rooms from '../data/Rooms.json'
+// import { RoomService } from '../services/roomService';
+// import { RoomValidator } from '../validators/roomValidator';
+// import { IdValidator } from '../validators/idValidator';
 
 
-import bodyParser from 'body-parser';
-import { UpdateBookingValidator } from '../validators/bookingValidator';
+// import bodyParser from 'body-parser';
+// import { UpdateBookingValidator } from '../validators/bookingValidator';
 
 
-export const roomsRouter = Router();
+// export const roomsRouter = Router();
 
-const jsonParser = bodyParser.json();
-
-
-roomsRouter.get('/', (req: Request, res : Response) => {
-    const roomService = new RoomService();
-    const roomList = roomService.fetchAll();
-    res.status(200).json(roomList);
-});
+// const jsonParser = bodyParser.json();
 
 
-roomsRouter.get('/:id', async(req : Request , res: Response)=> {
-    if(IdValidator(req.params.room_id)){
-        const roomService = new RoomService();
-        const room = await roomService.fetchById(parseInt(req.params.id));
-        return res.status(200).json(room)
-    }else{
-        return res.status(400).json({message: "Room Id not valid"})
-    }
+// roomsRouter.get('/', (req: Request, res : Response) => {
+//     const roomService = new RoomService();
+//     const roomList = roomService.fetchAll();
+//     res.status(200).json(roomList);
+// });
+
+
+// roomsRouter.get('/:id', async(req : Request , res: Response)=> {
+//     if(IdValidator(req.params.room_id)){
+//         const roomService = new RoomService();
+//         const room = await roomService.fetchById(parseInt(req.params.id));
+//         return res.status(200).json(room)
+//     }else{
+//         return res.status(400).json({message: "Room Id not valid"})
+//     }
     
-})
+// })
 
-roomsRouter.post('/', jsonParser , async(req : Request , res: Response) : Promise<any> => {
-    const roomService = new RoomService();
+// roomsRouter.post('/', jsonParser , async(req : Request , res: Response) : Promise<any> => {
+//     const roomService = new RoomService();
 
-    if(RoomValidator(req, res) && roomService.RoomExists(req.body.room_id) === "Id incorrecto"){
-        await roomService.create(req.body);
-        return res.status(201).json("Created");
+//     if(RoomValidator(req, res) && roomService.RoomExists(req.body.room_id) === "Id incorrecto"){
+//         await roomService.create(req.body);
+//         return res.status(201).json("Created");
 
-    }else{
-        return res.status(400).json({message: "No funciona"}) // Cambiar comentarios
-    }
+//     }else{
+//         return res.status(400).json({message: "No funciona"}) // Cambiar comentarios
+//     }
 
-})
+// })
 
-roomsRouter.put('/', jsonParser , async(req :Request , res : Response) : Promise<any> => {
-    const roomService = new RoomService();
-    if(UpdateBookingValidator(req, res)){
-        const updatedRoom = await roomService.update(req.body);
-        if(updatedRoom !== "Usuario no existente"){
-            return res.status(202).json(updatedRoom);
-        }else{
-            return res.status(400).json({ message: "Usuario no existente"})
-        }
+// roomsRouter.put('/', jsonParser , async(req :Request , res : Response) : Promise<any> => {
+//     const roomService = new RoomService();
+//     if(UpdateBookingValidator(req, res)){
+//         const updatedRoom = await roomService.update(req.body);
+//         if(updatedRoom !== "Usuario no existente"){
+//             return res.status(202).json(updatedRoom);
+//         }else{
+//             return res.status(400).json({ message: "Usuario no existente"})
+//         }
         
 
-    }else{
-        return res.status(400).json({message: "No funciona"})
-    }
-})
+//     }else{
+//         return res.status(400).json({message: "No funciona"})
+//     }
+// })
 
-roomsRouter.delete('/:id', jsonParser , async(req : Request , res : Response) : Promise<any> => {
-    const roomService = new RoomService();
-    if(roomService.RoomExists(req.params.id) !== "Id incorrecto"){
+// roomsRouter.delete('/:id', jsonParser , async(req : Request , res : Response) : Promise<any> => {
+//     const roomService = new RoomService();
+//     if(roomService.RoomExists(req.params.id) !== "Id incorrecto"){
 
-        const remainingList = typeof req.params.id !== "number" ? await roomService.deleteId(parseInt(req.params.id)) : await roomService.deleteId(req.params.id);
+//         const remainingList = typeof req.params.id !== "number" ? await roomService.deleteId(parseInt(req.params.id)) : await roomService.deleteId(req.params.id);
         
-        return res.status(202).json(remainingList);
-    }else{
-        return  res.status(400).json({message: "Id no existente"})
-    }
-})
+//         return res.status(202).json(remainingList);
+//     }else{
+//         return  res.status(400).json({message: "Id no existente"})
+//     }
+// })
